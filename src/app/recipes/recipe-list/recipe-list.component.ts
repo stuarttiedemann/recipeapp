525b07f2609d2c6ff9340267a218665bb5dadb9e
Stuart Tiedemann <tiedemannstuart@gmail.com>
2017-05-19 15:24:13 -0500

    build out recipe model, recipe list component recipe detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+      new Recipe('A Test Recipe', 'This is simply a test', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Cooking-Delicious-Korean-Food-Recipe-Asia-822090.jpg')
+    ];
   constructor() { }
 
   ngOnInit() {
